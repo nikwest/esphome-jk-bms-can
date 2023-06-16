505675334934f6297dd1323f417838734ec3c7bb
denac <Nico.Ackermann@vanderlande.com>
2023-06-16 17:27:07 +0200

    add uart message status

--- components/jk_bms/jk_bms.h
+++ components/jk_bms/jk_bms.h
@@ -333,7 +333,7 @@ class JkBms : public PollingComponent, public jk_modbus::JkModbusDevice {
   } cells_[24];
 
   bool enable_fake_traffic_;
-  bool uart_or_send_can_active_;
+  volatile bool uart_or_send_can_active_;
 
   void on_status_data_(const std::vector<uint8_t> &data);
   void publish_state_(binary_sensor::BinarySensor *binary_sensor, const bool &state);
