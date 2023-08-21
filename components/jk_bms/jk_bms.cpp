739d894438f564cb8a25f1432eaab47495f71660
denac <Nico.Ackermann@vanderlande.com>
2023-08-21 12:15:01 +0200

    add top_balancing_enabled flag

--- components/jk_bms/jk_bms.cpp
+++ components/jk_bms/jk_bms.cpp
@@ -386,7 +386,7 @@ void JkBms::on_status_data_(const std::vector<uint8_t> &data) {
 
   // 00 00 00 00 68 00 00 54 D1: End of frame
 
-  if (get_top_balancing_enabled() != this->balancing_switch_binary_sensor_.state) {
+  if (get_top_balancing_enabled() != this->balancing_switch_binary_sensor_->state) {
     if (get_top_balancing_enabled()) {
       sendBalancingOn();
     } else {
